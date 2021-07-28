6031dd6002aeab5424504ebb7ef448a64a1c6c76
Epersonf <epersoncmxf@gmail.com>
2021-07-28 17:44:27 -0300

    Negative hypergeometric

--- eProbAPI/discrete_distribution_functions.py
+++ eProbAPI/discrete_distribution_functions.py
@@ -1,6 +1,10 @@
 import math
-from eProbAPI.combinatoric_extra import c, p, factorial
+from eProbAPI.combinatoric_extra import c, factorial
 from eProbAPI.probability_function import ProbFunction
+
+
+def geometric(p: int):
+    return neg_binomial(1, p)
 
 
 def binomial(n: int, p: float) -> ProbFunction:
@@ -11,37 +15,28 @@ def binomial(n: int, p: float) -> ProbFunction:
     )
 
 
-def geometric(p: int):
+def neg_binomial(r: int, p: float) -> ProbFunction:
     return ProbFunction(
-        lambda k: p * (1 - p) ** (k - 1),
-        1 / p,
-        (1 - p) / (p ** 2)
+        lambda k: c(k - 1, r - 1) * p ** r * (1 - p) ** (k - r),
+        r / p,
+        (r * (1 - p)) / (p ** 2)
     )
 
 
-def hyper_geometric(N: int, r: int, n: int) -> ProbFunction:
-    p = r / N
+def hyper_geometric(N: int, K: int, n: int) -> ProbFunction:
+    p = K / N
     return ProbFunction(
-        lambda k: (c(r, k) * c(N - r, n - k)) / c(N, n),
+        lambda k: (c(K, k) * c(N - K, n - k)) / c(N, n),
         n * p,
         n * p * (1 - p) * (N - n) / (N - 1)
     )
 
 
-def hyper_geometric_perm(N: int, r: int, n: int) -> ProbFunction:
-    P = r / N
+def neg_hyper_geometric(N: int, K: int, r: int):
     return ProbFunction(
-        lambda k: c(r, k) * p(n, k) * p(N - n, r - k) / p(N, r),
-        n * P,
-        n * P * (1 - P) * (N - n) / (N - 1)
-    )
-
-
-def pascal(r: int, p: float) -> ProbFunction:
-    return ProbFunction(
-        lambda k: c(k - 1, r - 1) * p ** r * (1 - p) ** (k - r),
-        r / p,
-        (r * (1 - p)) / (p ** 2)
+        lambda k: c(k + r - 1, k) * c(N - r - k, K - k) / c(N, K),
+        r * K/(N - K + 1),
+        r * ((N + 1) * K)/((N - K + 1) * (N - K + 2)) * (1 - (r/(N - K + 1)))
     )
 
 
--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 from itertools import permutations, product, combinations, combinations_with_replacement
 from eProbAPI.combinatoric_extra import c, cr, p, pr
 from eProbAPI.discrete_prob_function_util import expected_value, variance
-from eProbAPI.discrete_distribution_functions import binomial, geometric, hyper_geometric, pascal, poisson, eperson
+from eProbAPI.discrete_distribution_functions import\
+    binomial, geometric, hyper_geometric, neg_binomial, poisson, neg_hyper_geometric
 from eProbAPI.probability_function import ProbFunction
 
-prob_func = geometric(1/4)
-
-print(prob_func.invoke(6))
